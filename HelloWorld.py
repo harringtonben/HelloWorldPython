4b9a11ad00666464ee12428c01ea40b26e3c8849
ben harrington <harringtonben@icloud.com>
2019-01-17 16:43:56 -0600

    finished hello world

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,39 @@
-print('Hello World!!')
-print(1 + 3)
-print(7 * 6)
-print('The End')
-greeting = 'hello'
-name = 'Bruce'
-# if we want a space, we can add that too
-print(greeting + ' ' + name)
+# Hellogreeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
+#
+# splitString = 'This string has been \nsplit over \nseveral \nlines'
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4"
+# print(tabbedString)
+#
+# anotherSplitString = """This string
+# has been
+# split over
+# several lines"""
+#
+# print(anotherSplitString)
+
+parrot = "norwegian blue"
+
+print(parrot)
+print(parrot[3])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+string1 = "These"
+string2 = "probably"
+
+print(string1 + string2)
+print("he's " "probably " "pining")
+print("Hello" * 5)
+today = "friday"
+
+print("day" in today)
+print("thur" in today)
+print("parrot" in "fjord")
---
+++ Variables.py
@@ -0,0 +1,26 @@
+# greeting = "Ben"
+# _myName = 'Ben'
+# age = 24
+#
+# print(age)
+#
+# print(greeting + age)
+
+# a = 12
+# b = 3
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+#
+#
+# for i in range(1, a//b):
+#     print(i)
+
+parrot = "Norwegian blue"
+
+print(parrot[3])
+
